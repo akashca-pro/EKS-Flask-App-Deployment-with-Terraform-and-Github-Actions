be04e4d1956b03c0b26bd42a8ca39acd058832b8
akashca-pro <akashca656@gmail.com>
2026-03-03 19:33:40 +0530

    config : Update EKS, VPC, and KMS Terraform modules, including node group user data, documentation, and CI/CD workflows, and add application dependencies.

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "ap-south-2"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name               = "eks-vpc"
+  cidr               = "10.0.0.0/16"
+  azs                = ["ap-south-2a", "ap-south-2b"]
+  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets     = ["10.0.101.0/24", "10.0.102.0/24"]
+  enable_nat_gateway = true
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "my-cluster"
+  cluster_version = "1.29"
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnets
+
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.micro"]
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
+    }
+  }
+}
